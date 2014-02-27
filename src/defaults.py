348e90efff8893c493bb3f10af4fd18b4d0a53a8
Kasper Peeters <kasper.peeters@phi-sci.com>
2014-02-27 16:05:54 +0000

    Added algorithm dispatch templates.

---
+++ src/defaults.py
@@ -0,0 +1,20 @@
+\prod{#}::Distributable.
+\prod{#}::IndexInherit.
+\prod{#}::CommutingAsProduct.
+\prod{#}::DependsInherit.
+\prod{#}::WeightInherit(label=all, type=Multiplicative).
+\prod{#}::NumericalFlat.
+
+\sum{#}::CommutingAsSum.
+\sum{#}::DependsInherit.
+\sum{#}::IndexInherit.
+\sum{#}::WeightInherit(label=all, type=Additive).
+
+\pow{#}::DependsInherit.
+
+\indexbracket{#}::Distributable.
+\indexbracket{#}::IndexInherit.
+\commutator{#}::IndexInherit.
+\commutator{#}::Derivative.
+\anticommutator{#}::IndexInherit.
+\anticommutator{#}::Derivative.
