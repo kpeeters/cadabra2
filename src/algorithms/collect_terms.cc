63aa7c6d86b88d645f6f6e1db8ac74c2d0b796d9
Kasper Peeters <kasper.peeters@fotoquirk.com>
2014-03-07 09:55:40 +0000

    Added collect_terms

---
+++ src/algorithms/collect_terms.cc
@@ -0,0 +1,112 @@
+
+#include "algorithms/collect_terms.hh"
+
+collect_terms::collect_terms(Kernel& k, exptree& tr)
+	: Algorithm(k, tr)
+	{
+	}
+
+bool collect_terms::can_apply(iterator st)
+	{
+	assert(tr.is_valid(st));
+	if(*st->name=="\\sum") return true;
+	return false;
+	}
+
+void collect_terms::fill_hash_map(iterator it)
+	{
+	fill_hash_map(tr.begin(it), tr.end(it));
+	}
+
+void collect_terms::fill_hash_map(sibling_iterator sib, sibling_iterator end)
+	{
+	term_hash.clear();
+	while(sib!=end) {
+		term_hash.insert(std::pair<hashval_t, sibling_iterator>(tr.calc_hash(sib), sib));
+		++sib;
+		}
+	}
+
+void collect_terms::remove_zeroed_terms(sibling_iterator from, sibling_iterator to)
+	{
+	// Remove all terms which have zero multiplier.
+	sibling_iterator one=from;
+	while(one!=to) {
+		if(*one->multiplier==0) 
+			one=tr.erase(one);
+		else if(*one->name=="\\sum" && *one->multiplier!=1) {
+			sibling_iterator oneit=tr.begin(one);
+			while(oneit!=tr.end(one)) {
+				multiply(oneit->multiplier, *one->multiplier);
+				++oneit;
+				}
+			one->multiplier=rat_set.insert(1).first;
+			++one;
+			}
+		else ++one;
+		}
+	}
+
+//algorithm::result_t collect_terms::apply(sibling_iterator& from, sibling_iterator& to)
+//	{
+//	assert(*tr.parent(from)->name=="\\sum");
+//	fill_hash_map(from, to);
+//	result_t res=collect_from_hash_map();
+//	remove_zeroed_terms(from, to);
+//	return res;
+//	}
+
+Algorithm::result_t collect_terms::apply(iterator& st)
+	{
+	assert(tr.is_valid(st));
+	assert(*st->name=="\\sum");
+	fill_hash_map(st);
+	result_t res=collect_from_hash_map();
+	remove_zeroed_terms(tr.begin(st), tr.end(st));
+
+	// If there is only one term left, flatten the tree.
+	if(tr.number_of_children(st)==1) {
+		tr.begin(st)->fl.bracket=st->fl.bracket;
+		tr.begin(st)->fl.parent_rel=st->fl.parent_rel;
+		tr.flatten(st);
+		st=tr.erase(st);
+		// We may have to propagate the multiplier up the tree to make it consistent.
+		pushup_multiplier(st);
+		}
+	else if(tr.number_of_children(st)==0) {
+//		zero(st->multiplier);
+		node_zero(st);
+		}
+	return res;
+	}
+
+Algorithm::result_t collect_terms::collect_from_hash_map()
+	{
+	result_t res=l_no_action;
+	term_hash_iterator_t ht=term_hash.begin();
+	while(ht!=term_hash.end()) {
+		hashval_t curr=ht->first;  // hash value of the current set of terms
+		term_hash_iterator_t thisbin1=ht, thisbin2;
+		while(thisbin1!=term_hash.end() && thisbin1->first==curr) {
+			thisbin2=thisbin1;
+			++thisbin2;
+			while(thisbin2!=term_hash.end() && thisbin2->first==curr) {
+				if(subtree_exact_equal(&kernel.properties, (*thisbin1).second, (*thisbin2).second, -2, true, 0, true)) {
+					res=l_applied;
+					add((*thisbin1).second->multiplier, *((*thisbin2).second->multiplier));
+					zero((*thisbin2).second->multiplier);
+					term_hash_iterator_t tmp=thisbin2;
+					++tmp;
+					term_hash.erase(thisbin2);
+					thisbin2=tmp;
+					expression_modified=true;
+					}
+				else ++thisbin2;
+				}
+			++thisbin1;
+			}
+		ht=thisbin1;
+		}
+		
+	return res;
+	}
---
+++ src/algorithms/collect_terms.hh
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "Algorithm.hh"
+
+class collect_terms : public Algorithm {
+	public:
+		collect_terms(Kernel&, exptree&);
+
+		virtual bool     can_apply(iterator);
+		virtual result_t apply(iterator&);
+
+		void  fill_hash_map(iterator);
+		void  fill_hash_map(sibling_iterator, sibling_iterator);
+	private:
+		result_t collect_from_hash_map();
+		void remove_zeroed_terms(sibling_iterator, sibling_iterator);
+
+		typedef std::multimap<hashval_t, sibling_iterator> term_hash_t;
+		typedef term_hash_t::iterator                      term_hash_iterator_t;
+
+		term_hash_t term_hash;
+};
+
