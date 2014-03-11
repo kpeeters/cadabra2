1da783e2f4986097ec4d09e9f5fd9078d5f59a71
Kasper Peeters <kasper.peeters@phi-sci.com>
2014-03-11 00:14:59 +0000

    Started work on GUI

---
+++ gui/common/test.cc
@@ -0,0 +1,12 @@
+
+#include "TeXEngine.hh"
+
+int main()
+	{
+	TeXEngine te;
+
+	TeXEngine::TeXRequest *req1=te.checkin("$e = mc^2$");
+	TeXEngine::TeXRequest *req2=te.checkin("$\int_{-\infty}^{\infty} e^{-ax^2} {\rm d}x = \sqrt{\frac{\pi}{a}}$");
+
+	te.convert_all();
+	}
