038bd8cf79adf4f6d83e93d62fd3a39413faa2db
Kasper Peeters <kasper.peeters@phi-sci.com>
2014-03-18 19:39:53 +0000

    Updated server

--- server/Server.cc
+++ server/Server.cc
@@ -1,7 +1,25 @@
 
 #include <boost/python.hpp>
+#include <zmq.hpp>
 
-Server::Server()
+Server::Server(const std::string& socket)
 	{
+	socket_name=socket;
 	}
 
+void Server::run() 
+	{
+	zmq::context_t context(1);
+	zmq::socket_t  socket(context, ZMQ_REP);
+	socket.bind(socket_name);
+
+	while(true) {
+		zmq::message_t request;
+		socket.recv (&request);
+		std::cout << "Received Hello" << std::endl;
+
+		zmq::message_t reply (5);
+		memcpy ((void *) reply.data (), "World", 5);
+		socket.send (reply);
+		}	
+	}
