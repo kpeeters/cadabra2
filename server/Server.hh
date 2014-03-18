f339e85b61227b2622a0f1b69b5d49e744e65fbd
Kasper Peeters <kasper.peeters@phi-sci.com>
2014-03-18 19:39:53 +0000

    Updated server

--- server/Server.hh
+++ server/Server.hh
@@ -5,8 +5,16 @@
 // (or other language) code.
 //
 // Every block is run inside its own Python local scope. 
+//
+// The server uses a ... 0mq socket to listen for incoming requests.
+
 
 class Server {
 	public:
+		Server(const std::string& socket);
 
+		void run();
+
+	private:
+		std::string socket_name;
 };
