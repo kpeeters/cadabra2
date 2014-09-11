46037db69fa2c89b1ea52abc73a5efb19af26c57
Kasper Peeters <kasper.peeters@phi-sci.com>
2014-09-11 12:10:13 +0100

    Working basics of a client base class

--- server/test_client.cc
+++ server/test_client.cc
@@ -3,15 +3,65 @@
 // some I/O with a cadabra server.
 
 #include "Client.hh"
+#include <thread>
 
 class MyClient : public cadabra::Client {
 	public:
-		
+		void on_connect();
+		void on_disconnect();
+		void on_network_error();
+		void on_progress();
+		void before_tree_change(ActionBase&);
+		void after_tree_change(ActionBase&);
 };
+
+class UI {
+	public:
+		void run();
+};
+
+void MyClient::on_connect() 
+	{
+	std::cout << "connected to server" << std::endl;
+	}
+
+void MyClient::on_disconnect() 
+	{
+	std::cout << "disconnected from server" << std::endl;
+	}
+
+void MyClient::on_network_error()
+	{
+	std::cout << "network error" << std::endl;
+	}
+
+void MyClient::on_progress() 
+	{
+	}
+
+void MyClient::before_tree_change(ActionBase& ab)
+	{
+	}
+
+void MyClient::after_tree_change(ActionBase& ab)
+	{
+	}
+
+void UI::run() 
+	{
+	sleep(10);
+	}
 
 int main(int, char **)
 	{
 	MyClient client;
+	UI       ui;
 
-	client.run();
+	// Spawn two threads.
+	std::thread client_thread(&MyClient::run, client);
+	std::thread ui_thread(&UI::run, ui);
+
+	// Wait for all threads to finish.
+	client_thread.join();
+	ui_thread.join();
 	}
