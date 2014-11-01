20e124e7857a5e832738ab24eb82718cdef9cb23
Kasper Peeters <kasper.peeters@phi-sci.com>
2014-11-01 19:07:59 +0000

    Added Cadabra OSX frontend skeleton

---
+++ frontend/osx/Cadabra/Cadabra/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Cadabra
+//
+//  Created by Kasper Peeters on 01/11/2014.
+//  Copyright (c) 2014 phi-sci. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
