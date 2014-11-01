30f842c17fb5829df5e2b164fdfa761e161332c9
Kasper Peeters <kasper.peeters@phi-sci.com>
2014-11-01 19:07:59 +0000

    Added Cadabra OSX frontend skeleton

---
+++ frontend/osx/Cadabra/Cadabra/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cadabra
+//
+//  Created by Kasper Peeters on 01/11/2014.
+//  Copyright (c) 2014 phi-sci. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ frontend/osx/Cadabra/Cadabra/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Cadabra
+//
+//  Created by Kasper Peeters on 01/11/2014.
+//  Copyright (c) 2014 phi-sci. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
