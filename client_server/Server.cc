979b09f0a803ca60323611c2d5618655a34f6a6f
Kasper Peeters <kasper.peeters@fotoquirk.com>
2014-12-17 18:30:11 +0000

    Displaying error messages

--- client_server/Server.cc
+++ client_server/Server.cc
@@ -290,6 +290,7 @@ void Server::on_block_finished(Block blk)
 
 	json["header"]=header;
 	json["content"]=content;
+	json["type"]="response";
 
 	std::ostringstream str;
 	str << json << std::endl;
@@ -310,6 +311,7 @@ void Server::on_block_error(Block blk)
 
 	json["header"]=header;
 	json["content"]=content;
+	json["type"]="error";
 
 	std::ostringstream str;
 	str << json << std::endl;
