88693b8368c631d9442843af37c40739ed47b949
Kasper Peeters <kasper.peeters@phi-sci.com>
2015-03-01 20:20:03 +0000

    Metric added, canonicalise test now passes

---
+++ core/properties/Metric.cc
@@ -0,0 +1,31 @@
+
+#include "Exceptions.hh"
+#include "properties/Metric.hh"
+
+Metric::Metric()
+	{
+	tab_t tab;
+	tab.add_box(0,0);
+	tab.add_box(0,1);
+	tabs.push_back(tab);
+	}
+
+std::string Metric::name() const
+	{
+	return "Metric";
+	}
+
+bool Metric::parse(const Properties&, keyval_t& keyvals) 
+	{
+	keyval_t::const_iterator kv=keyvals.find("signature");
+	signature=1;
+	if(kv!=keyvals.end())
+		signature=to_long(*(kv->second->multiplier));
+	return true;
+	}
+
+void Metric::validate(const Properties& props, const exptree& tr) const
+	{
+	if(tr.number_of_children(tr.begin())!=2) 
+		throw ArgumentException("Metric: needs exactly 2 indices.");
+	}
