dbb91c8b0747ea86149c19e70f3913ac2bfd80e3
Kasper Peeters <kasper.peeters@fotoquirk.com>
2015-03-26 16:16:40 +0000

    Passing lists of ints works

--- core/PythonCdb.cc
+++ core/PythonCdb.cc
@@ -550,6 +550,19 @@ Ex *dispatch_1_ex(Ex *ex, bool deep, bool repeat, unsigned int depth, Args... ar
 	return ex;
 	}
 
+template<class F, typename Args>
+Ex *dispatch_1_ex_new(Ex *ex, Args args, bool deep, bool repeat, unsigned int depth)
+	{
+	F algo(*get_kernel_from_scope(), ex->tree, args);
+
+	exptree::iterator it=ex->tree.begin().begin();
+	
+	ex->reset_state();
+	ex->update_state(algo.apply_generic(it, deep, repeat, depth));
+	
+	return ex;
+	}
+
 template<class F>
 Ex *dispatch_1_string(const std::string& ex, bool deep, bool repeat, unsigned int depth)
 	{
@@ -644,6 +657,59 @@ void def_prop()
 	}
 
 
+// Converter from Python iterables to std::vector and friends.
+// http://stackoverflow.com/questions/15842126/
+
+class iterable_converter {
+	public:
+		template <typename Container>
+		iterable_converter&
+		from_python() 
+			{
+			boost::python::converter::registry::push_back(
+				&iterable_converter::convertible,
+				&iterable_converter::construct<Container>,
+				boost::python::type_id<Container>());
+
+			return *this;
+			}
+
+		static void* convertible(PyObject* object)
+			{
+			return PyObject_GetIter(object) ? object : NULL;
+			}
+
+		template <typename Container>
+		static void construct(PyObject* object, boost::python::converter::rvalue_from_python_stage1_data* data)
+			{
+			namespace python = boost::python;
+			// Object is a borrowed reference, so create a handle indicting it is
+			// borrowed for proper reference counting.
+			python::handle<> handle(python::borrowed(object));
+			
+			// Obtain a handle to the memory block that the converter has allocated
+			// for the C++ type.
+			typedef python::converter::rvalue_from_python_storage<Container> storage_type;
+			void* storage = reinterpret_cast<storage_type*>(data)->storage.bytes;
+			
+			typedef python::stl_input_iterator<typename Container::value_type> iterator;
+			
+			// Allocate the C++ type into the converter's memory block, and assign
+			// its handle to the converter's convertible variable.  The C++
+			// container is populated by passing the begin and end iterators of
+			// the python object to the container's constructor.
+			new (storage) Container(
+				iterator(python::object(handle)), // begin
+				iterator());                      // end
+			data->convertible = storage;
+			}
+};
+
+std::string ctest(std::vector<int> vec) 
+	{
+	return "yes";
+	}
+
 // Entry point for registration of the Cadabra Python module. 
 // This registers the main Ex class which wraps Cadabra expressions, as well
 // as the various algorithms that can act on these and the properties that can
@@ -732,9 +798,14 @@ BOOST_PYTHON_MODULE(cadabra2)
 		  arg("expand")=true,arg("use_gendelta")=false),
 		 return_internal_reference<1>() );
 
-	def("keep_terms", &dispatch_1_ex<keep_terms, boost::python::slice>,
-		 (arg("ex"),arg("deep")=true,arg("repeat")=false,arg("depth")=0,
-		  arg("terms")),
+	// Automatically convert Python sets and so on of integers to std::vector.
+	iterable_converter().from_python<std::vector<int> >();
+
+	def("ctest", &ctest);
+ 
+	def("keep_terms", &dispatch_1_ex_new<keep_terms, std::vector<int> >,
+		 (arg("ex"), 
+		  arg("terms"),arg("deep")=true,arg("repeat")=false,arg("depth")=0),
 		 return_internal_reference<1>() );
 
 	// Algorithms which take a second Ex as argument.
