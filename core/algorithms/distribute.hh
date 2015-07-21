58cb3feaec1b804eb9e9005c4293f20e649ebd8c
Kasper Peeters <kasper.peeters@phi-sci.com>
2015-07-21 16:37:17 +0200

    Experimenting with doxygen features

--- core/algorithms/distribute.hh
+++ core/algorithms/distribute.hh
@@ -3,6 +3,9 @@
 
 #include "Algorithm.hh"
 
+/// Distribute factors over a sum, that is,
+/// \f$ (A+B) C \rightarrow A C + B C \f$
+
 class distribute : public Algorithm {
 	public:
 		distribute(Kernel&, exptree&);
