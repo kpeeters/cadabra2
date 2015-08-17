ef08e9a439efb7d49b0a637be177c88ab33ba18e
Kasper Peeters <kasper.peeters@fotoquirk.com>
2015-08-17 09:58:28 +0200

    Added send_message on server

--- client_server/Server.hh
+++ client_server/Server.hh
@@ -1,17 +1,6 @@
 
 #pragma once
 
-// A Cadabra server object. An object of this type can run blocks of Python
-// code (or perhaps other languages later).
-//
-// The server uses websocket++ sockets to listen for incoming requests.
-// For testing purposes, look at test_server.cc and test_talk_to_server.cc.
-//
-// Every block is run on the same Python scope. They run sequentially,
-// one at a time, on a thread separate from the websocket++ main loop.
-// When the Python code finishes (or when it is interrupted), this thread
-// locks the socket_mutex and calls on_block_finished().
-
 #include <string>
 #include <boost/uuid/uuid.hpp>
 #include <websocketpp/server.hpp>
@@ -20,22 +9,39 @@
 #include <future>
 #include <boost/python.hpp>
 
+/// \ingroup clientserver
+///
+/// Object representing a Cadabra server, capable of receiving messages
+/// on a websocket, running Python code, and sending output back to the
+/// client.
+///
+/// Contains the logic to intercept raw Python output but also provides
+/// functionality to the Python side which enables clients to send 
+/// various objects in JSON encoded form. See Server::on_block_finished
+/// for the format of these messages.
+///
+/// Every block is run on the same Python scope. They run sequentially,
+/// one at a time, on a thread separate from the websocket++ main loop.
+/// When the Python code finishes (or when it is interrupted), this thread
+/// locks the socket_mutex and calls on_block_finished().
+
 class Server {
 	public:
 		Server();
+		Server(const Server&)=delete;
 		Server(const std::string& socket);
 		~Server();
 
-		// The only user-visible part: just instantiate a server object and
-		// start it with run(). This will not return until the server has
-		// been shut down.
+		/// The only user-visible part: just instantiate a server object and
+		/// start it with run(). This will not return until the server has
+		/// been shut down.
 		void run();
 
 
-		// Python output catching. We implement this in a C++ class
-		// because we want to be able to catch each line of output
-		// separately, and perhaps add additional functionality to send
-		// out-of-band messages to the client later.
+		/// Python output catching. We implement this in a C++ class
+		/// because we want to be able to catch each line of output
+		/// separately, and perhaps add additional functionality to send
+		/// out-of-band messages to the client later.
 
 		class CatchOutput {
 			public:
@@ -100,7 +106,9 @@ class Server {
 				std::string                 error;
 				uint64_t                    id;
 		};
-		std::queue<Block>        block_queue;
+		std::queue<Block>           block_queue;
+		websocketpp::connection_hdl current_hdl;
+		uint64_t                    current_id;
 
 		// Run a piece of Python code. This is called from a separate
 		// thread constructed by on_message().
@@ -110,21 +118,36 @@ class Server {
 		// Python. Mimics the functionality in core/cadabra2.
 		std::string              pre_parse(const std::string&);
 
-		// Called by the run_block() thread upon completion of the task.
-		// Handles communication of the result back to the client.
+		/// Called by the run_block() thread upon completion of the
+		/// task.  Handles communication of the result back to the
+		/// client in JSON format. This is always of the form
+		///
+		///      { "header":   { "cell_id": "..." },
+      ///        "content":  { "output":  "..." },
+      ///        "msg_type": "..." 
+      ///      }
+		///
+		/// msg_type can be "output", "latex", "image_png" and so on,
+		/// corresponding to the possible values of DataCell::CellType.
+
 		void                     on_block_finished(Block);
 		void                     on_block_error(Block);
 		void                     on_kernel_fault(Block);
 
-		// Halt the currently running block and prevent execution of any
-		// further blocks that may still be on the queue.
+		/// Raw code to send a string (which must be JSON formatted) as
+		/// a message to the client.
+		void                     send_message(const std::string& output, const std::string& msg_type);
+		void                     send_json_message(const std::string&);
+
+		/// Halt the currently running block and prevent execution of any
+		/// further blocks that may still be on the queue.
 		void                     stop_block(); 
 		bool                     started;
 		std::future<std::string> job;
 
-		// Takes a JSON encoded message and performs the required action to process it.
-		// Where applicable these messages are compatible with IPython's message types,
-		// http://ipython.org/ipython-doc/dev/development/messaging.html
+		/// Takes a JSON encoded message and performs the required action to process it.
+		/// Where applicable these messages are compatible with IPython's message types,
+		/// http://ipython.org/ipython-doc/dev/development/messaging.html
 
 		void dispatch_message(websocketpp::connection_hdl, const std::string& json_string);
 
