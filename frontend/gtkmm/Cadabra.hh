f5e87aefd0f6feb88f1f777bd796c97b4e376f7d
Kasper Peeters <kasper.peeters@phi-sci.com>
2015-09-03 13:25:32 +0100

    Proper gtkapp

---
+++ frontend/gtkmm/Cadabra.hh
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include <gtkmm/application.h>
+#include "NotebookWindow.hh"
+#include "ComputeThread.hh"
+
+/// The Cadabra notebook application.
+
+class Cadabra : public Gtk::Application {
+	public:
+		static Glib::RefPtr<Cadabra> create();
+
+	protected:
+		Cadabra();
+
+		void on_activate() override;
+		void on_open(const Gio::Application::type_vec_files& files, const Glib::ustring& hint) override;
+
+	private:
+		cadabra::NotebookWindow nw;
+		cadabra::ComputeThread  compute;
+		std::thread             compute_thread;
+};
--- frontend/gtkmm/main.cc
+++ frontend/gtkmm/main.cc
@@ -1,22 +1,10 @@
 
 #include "Log.hh"
-#include "ComputeThread.hh"
-#include "NotebookWindow.hh"
+#include "Cadabra.hh"
 
 #include <gtkmm/application.h>
 #include <gtkmm/cssprovider.h>
 #include <gtkmm/label.h>
-
-#include <signal.h>
-
-// Signal handler for ctrl-C
-
-cadabra::NotebookWindow *signal_window;
-
-void signal_handler(int signal)
-	{
-	signal_window->close();
-	}
 
 // Cadabra frontend with GTK+ interface (using gtkmm). 
 // Makes use of the client classes in the client_server directory.
@@ -26,34 +14,18 @@ int main(int argc, char **argv)
 	boost::log::add_common_attributes();
 
 	try {
-		// Create the ui material.
-		Glib::RefPtr<Gtk::Application> app =
-			Gtk::Application::create(argc, argv, "com.phi-sci.cadabra.cadabra-gtk");
-
-		// Setup windows.
-		cadabra::NotebookWindow nw;
-		cadabra::ComputeThread compute(&nw, nw);
- 
-		// Create and start the compute/network thread.
-		std::thread compute_thread(&cadabra::ComputeThread::run, &compute);
-		
-		// Connect the two threads.
-		nw.set_compute_thread(&compute);
-
-		// Setup ctrl-C handler so we can shut down gracefully (i.e. ask
-		// for confirmation, shut down server).
-		signal_window = &nw;
-		signal(SIGINT, signal_handler);
-
-		// Start the ui in the main thread.
-		app->run(nw);
+		auto application = Cadabra::create();
+		const int status = application->run(argc, argv);
+		return status;
 
 		// The window has been closed; stop the compute logic and join
 		// that thread waiting for it to complete.
-		compute.terminate();
-		compute_thread.join();
+		// FIXME: move to application
+//		compute.terminate();
+//		compute_thread.join();
 		}
 	catch(Glib::Error& er) {
 		std::cerr << er.what() << std::endl;
+		return -1;
 		}
 	}
