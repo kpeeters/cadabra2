f48df80aa20f1ee0345df3705e78332cf781736e
Kasper Peeters <kasper.peeters@fotoquirk.com>
2015-10-07 09:20:28 +0100

    Fixing property display

---
+++ core/properties/Integral.hh
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "Props.hh"
+
+// Turns a symbol into an integration function, which takes two arguments,
+// the integrand and the coordinate over which to integrate, optionally
+// with a range.
+
+class Integral : public property {
+	public:
+		virtual ~Integral() {};
+		
+		virtual std::string name() const;
+		virtual bool        parse(const Properties&, keyval_t& keyvals) override;
+		virtual void        display(std::ostream&) const;
+};
