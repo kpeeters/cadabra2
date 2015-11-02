5ff53c6ffc624292e26ca7703b3ce9d63e416ff5
Kasper Peeters <kasper.peeters@phi-sci.com>
2015-11-02 20:51:50 +0000

    Fixed LaTeXForm

--- core/properties/LaTeXForm.cc
+++ core/properties/LaTeXForm.cc
@@ -11,18 +11,22 @@ std::string LaTeXForm::unnamed_argument() const
 	return "latex";
 	}
 
-bool LaTeXForm::parse(Ex& tr, Ex::iterator pat, Ex::iterator prop, keyval_t& keyvals)
+bool LaTeXForm::parse(const Properties& p, keyval_t& keyvals)
 	{
 	keyval_t::const_iterator kv=keyvals.find("latex");
-	if(kv!=keyvals.end()) latex_=*(kv->second->name);
-	// FIXME: handle errors.
+	if(kv!=keyvals.end()) {
+		std::cerr << *(kv->second->name) << std::endl;
+		latex_=*(kv->second->name);
+		}
+	std::cerr << latex_ << std::endl;
+	// Strip quotes. FIXME: handle errors.
 	latex_=latex_.substr(1,latex_.size()-2);
+	std::cerr << latex_ << std::endl;
 	return true;
 	}
 
 std::string LaTeXForm::latex_form() const
 	{
-	std::cerr << "printing " << latex_ << std::endl;
 	return latex_;
 	}
 
--- core/properties/LaTeXForm.hh
+++ core/properties/LaTeXForm.hh
@@ -5,9 +5,9 @@
 
 class LaTeXForm : virtual public property {
 	public:
-		virtual std::string name() const;
-		virtual bool parse(Ex&, Ex::iterator, Ex::iterator, keyval_t&);
-		virtual std::string unnamed_argument() const;
+		virtual std::string name() const override;
+		virtual bool parse(const Properties&, keyval_t&) override;
+		virtual std::string unnamed_argument() const override;
 
 		std::string latex_form() const;
 	private:
