b4fc8796d1a1f81f4c2a93b3c2322c932584243c
Kasper Peeters <kasper.peeters@fotoquirk.com>
2016-03-04 19:12:10 +0000

    Localised possible issue when allow for negative indices.

--- core/Compare.cc
+++ core/Compare.cc
@@ -262,7 +262,7 @@ bool tree_exact_equal_mod_prel_obj::operator()(const Ex& one, const Ex& two) con
 
 bool tree_exact_less_for_indexmap_obj::operator()(const Ex& one, const Ex& two) const
 	{
-	return tree_exact_less(0, one, two, 0, true, -2, true);
+	return tree_exact_less(0, one, two, 0, true, 0 /* was -2 */, true); 
 	}
 
 //bool operator==(const Ex& first, const Ex& second)
