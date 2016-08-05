563fa16f70fe41d37196225162d14587fe33db4a
Kasper Peeters <kasper.peeters@phi-sci.com>
2016-08-05 16:33:48 +0100

    Do not mess with python globals unless we are up and running

--- core/PythonCdb.cc
+++ core/PythonCdb.cc
@@ -361,31 +361,43 @@ std::shared_ptr<Ex> make_Ex_from_string(const std::string& ex_, bool make_ref=tr
 	std::stringstream str(ex_);
 
 	try {
+	  //	  std::cerr << "parsing " << ex_ << std::endl;
 		str >> parser;
 //		Ex::print_recursive_treeform(std::cout, ptr->begin());
 		}
 	catch(std::exception& except) {
+	  //	  std::cerr << "cannot parse " << ex_ << std::endl;
 		throw ParseException("Cannot parse");
 		}
 	parser.finalise();
+	//	std::cerr << "finalised" << std::endl;
 
 	// First pull in any expressions referred to with @(...) notation, because the
 	// full expression may not have consistent indices otherwise.
 	pull_in(ptr);
+	//	std::cerr << "pulled in" << std::endl;
    
 	// Basic cleanup of rationals and subtractions, followed by
    // cleanup of nested sums and products.
 	pre_clean_dispatch_deep(*get_kernel_from_scope(), *ptr);
 	cleanup_dispatch_deep(*get_kernel_from_scope(), *ptr);
 	call_post_process(*ptr);
+	//	std::cerr << "cleaned up" << std::endl;
 
 	// The local variable stack is not writeable so we cannot insert '_'
 	// as a local variable. Instead, we push it onto the global stack.
 	
 	if(make_ref) {
-		boost::python::object globals(boost::python::borrowed(PyEval_GetGlobals()));
-		globals["_"]=ptr;
-		}
+	  //	  std::cerr << "making ref" << std::endl;
+	  auto globals_c = PyEval_GetGlobals();
+	  if(globals_c!=NULL) {
+	    // std::cerr << "globals_c non-null" << std::endl;
+	    boost::python::object globals(boost::python::borrowed(globals_c));
+	    // std::cerr << "inserting ref" << std::endl;
+	    globals["_"]=ptr;
+	  }
+	}
+	// std::cerr << "make_ex_from_string done" << std::endl;
 	return ptr;
 	}
 
@@ -630,44 +642,44 @@ void inject_defaults(Kernel *k)
 	// Create and inject properties; these then get owned by the kernel.
 	post_process_enabled=false;
 
-	inject_property(k, new Distributable(),      make_Ex_from_string("\\prod{#}"), 0);
-	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\prod{#}"), 0);
-	inject_property(k, new CommutingAsProduct(), make_Ex_from_string("\\prod{#}"), 0);
-	inject_property(k, new DependsInherit(),     make_Ex_from_string("\\prod{#}"), 0);
-	inject_property(k, new NumericalFlat(),      make_Ex_from_string("\\prod{#}"), 0);
+	inject_property(k, new Distributable(),      make_Ex_from_string("\\prod{#}",false), 0);
+	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\prod{#}",false), 0);
+	inject_property(k, new CommutingAsProduct(), make_Ex_from_string("\\prod{#}",false), 0);
+	inject_property(k, new DependsInherit(),     make_Ex_from_string("\\prod{#}",false), 0);
+	inject_property(k, new NumericalFlat(),      make_Ex_from_string("\\prod{#}",false), 0);
 	auto wi2=new WeightInherit();
 	wi2->combination_type = WeightInherit::multiplicative;
-	auto wa2=make_Ex_from_string("label=all, type=multiplicative");
-	inject_property(k, wi2,                      make_Ex_from_string("\\prod{#}"), wa2);
+	auto wa2=make_Ex_from_string("label=all, type=multiplicative", false);
+	inject_property(k, wi2,                      make_Ex_from_string("\\prod{#}",false), wa2);
 
-	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\sum{#}"), 0);
-	inject_property(k, new CommutingAsSum(),     make_Ex_from_string("\\sum{#}"), 0);
+	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\sum{#}",false), 0);
+	inject_property(k, new CommutingAsSum(),     make_Ex_from_string("\\sum{#}",false), 0);
 	auto wi=new WeightInherit();
-	auto wa=make_Ex_from_string("label=all, type=additive");
-	inject_property(k, wi,                       make_Ex_from_string("\\sum{#}"), wa);
+	auto wa=make_Ex_from_string("label=all, type=additive", false);
+	inject_property(k, wi,                       make_Ex_from_string("\\sum{#}", false), wa);
 
-	inject_property(k, new Derivative(),         make_Ex_from_string("\\commutator{#}"), 0);
-	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\commutator{#}"), 0);
+	inject_property(k, new Derivative(),         make_Ex_from_string("\\commutator{#}",false), 0);
+	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\commutator{#}",false), 0);
 
-	inject_property(k, new Derivative(),         make_Ex_from_string("\\anticommutator{#}"), 0);
-	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\anticommutator{#}"), 0);
+	inject_property(k, new Derivative(),         make_Ex_from_string("\\anticommutator{#}",false), 0);
+	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\anticommutator{#}",false), 0);
 
-	inject_property(k, new Distributable(),      make_Ex_from_string("\\indexbracket{#}"), 0);
-	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\indexbracket{#}"), 0);
+	inject_property(k, new Distributable(),      make_Ex_from_string("\\indexbracket{#}",false), 0);
+	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\indexbracket{#}",false), 0);
 
-	inject_property(k, new DependsInherit(),     make_Ex_from_string("\\pow{#}"), 0);
+	inject_property(k, new DependsInherit(),     make_Ex_from_string("\\pow{#}",false), 0);
 
-	inject_property(k, new NumericalFlat(),      make_Ex_from_string("\\int{#}"), 0);
-	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\int{#}"), 0);
+	inject_property(k, new NumericalFlat(),      make_Ex_from_string("\\int{#}",false), 0);
+	inject_property(k, new IndexInherit(),       make_Ex_from_string("\\int{#}",false), 0);
 
 	// Accents, necessary for proper display.
-	inject_property(k, new Accent(),             make_Ex_from_string("\\hat{#}"), 0);
-	inject_property(k, new Accent(),             make_Ex_from_string("\\bar{#}"), 0);
-	inject_property(k, new Accent(),             make_Ex_from_string("\\overline{#}"), 0);
-	inject_property(k, new Accent(),             make_Ex_from_string("\\tilde{#}"), 0);
+	inject_property(k, new Accent(),             make_Ex_from_string("\\hat{#}",false), 0);
+	inject_property(k, new Accent(),             make_Ex_from_string("\\bar{#}",false), 0);
+	inject_property(k, new Accent(),             make_Ex_from_string("\\overline{#}",false), 0);
+	inject_property(k, new Accent(),             make_Ex_from_string("\\tilde{#}",false), 0);
 
 	post_process_enabled=true;
-//	inject_property(k, new Integral(),           make_Ex_from_string("\\int{#}"), 0);
+//	inject_property(k, new Integral(),           make_Ex_from_string("\\int{#}",false), 0);
 	}
 
 void inject_property(Kernel *kernel, property *prop, std::shared_ptr<Ex> ex, std::shared_ptr<Ex> param)
@@ -970,7 +982,7 @@ BOOST_PYTHON_MODULE(cadabra2)
 //	class_<Ex, std::shared_ptr<Ex> > pyEx("Ex", boost::python::no_init);
 	class_<Ex, std::shared_ptr<Ex> >("Ex", boost::python::no_init)
 		.def("__init__", boost::python::make_constructor(&construct_Ex_from_string))
-	   .def("__init__", boost::python::make_constructor(&construct_Ex_from_string_2))
+	        .def("__init__", boost::python::make_constructor(&construct_Ex_from_string_2))
 		.def("__init__", boost::python::make_constructor(&construct_Ex_from_int))
 		.def("__init__", boost::python::make_constructor(&construct_Ex_from_int_2))
 		.def("__str__",  &Ex_str_)
@@ -1057,9 +1069,9 @@ BOOST_PYTHON_MODULE(cadabra2)
 		 return_internal_reference<1>() );
 
 	def("eliminate_metric", &dispatch_ex<eliminate_metric, Ex&>, 
-		 (arg("ex"),arg("preferred")=make_Ex_from_string(""),
-		  arg("deep")=false,arg("repeat")=false,arg("depth")=0),
-		 return_internal_reference<1>() );
+	    (arg("ex"),arg("preferred")=new Ex(), 
+	     arg("deep")=false,arg("repeat")=false,arg("depth")=0),
+	    return_internal_reference<1>() );
 
 	def("keep_weight", &dispatch_ex<keep_weight, Ex&>, 
 		 (arg("ex"),arg("condition"),
@@ -1085,7 +1097,7 @@ BOOST_PYTHON_MODULE(cadabra2)
 	iterable_converter().from_python<std::vector<int> >();
 
 	def("einsteinify", &dispatch_ex<einsteinify, Ex&>,
-		 (arg("ex"), arg("metric")=make_Ex_from_string(""),
+	    (arg("ex"), arg("metric")=new Ex(),
 		  arg("deep")=false,arg("repeat")=false,arg("depth")=0),
 		 return_internal_reference<1>() );
 		  
@@ -1188,7 +1200,6 @@ BOOST_PYTHON_MODULE(cadabra2)
 		  arg("deep")=true,arg("repeat")=false,arg("depth")=0),
 		 return_internal_reference<1>() );
 
-
 	// Properties are declared as objects on the Python side as well. They all take two
 	// Ex objects as constructor parameters: the first one is the object(s) to which the
 	// property is attached, the second one is the argument list (represented as an Ex).
@@ -1272,7 +1283,6 @@ BOOST_PYTHON_MODULE(cadabra2)
 	pyInternalError.def("__str__", &InternalError::py_what);
 	register_exception_translator<InternalError>(&translate_InternalError);
 	
-
 #if BOOST_VERSION >= 106000
 	boost::python::register_ptr_to_python<std::shared_ptr<Ex> >();
 	//	boost::python::register_ptr_to_python<std::shared_ptr<Property> >();
