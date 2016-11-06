af881c9d08b6fc1501cbbaafdfc0aaf45c16c415
Kasper Peeters <kasper.peeters@phi-sci.com>
2016-11-06 12:46:57 +0000

    Fixing icons hopefully

--- config/buildbot.sh
+++ config/buildbot.sh
@@ -2,32 +2,54 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function runbuild {
-	 # start the VM and wait for it to come up
+	 # Start the VM and wait for it to come up.
 	 echo "Starting build bot for $1, ssh port $3"
 	 ssh buildbothost "nohup VBoxHeadless -s '$1' > /tmp/buildbot.out 2> /tmp/buildbot.err < /dev/null &"
 	 sleep 5
 
-	 # setup the tunnel to the VM
+	 # Setup the tunnel to the VM; a local port $3 gets mapped
+	 # to a port $3 on the build host, which is forwarded by 
+	 # virtualbox to the ssh port of the build bot.
+
 	 echo 'Going to start build process...'
 	 ssh -M -S my-ctrl-socket -fnNT -L $3:localhost:$3 buildbothost
 	 
-	 # execute build commands on the VM
-	 #ssh -tt buildbot cadabra2/config/buildpkg
+	 # Execute build commands on the VM, using the build script
+	 # on the local machine (as the build bot may not have an
+	 # up-to-date build script yet).
+
 	 ssh -tt -p $3 buildbot "bash -s" -- < ${DIR}/buildpkg.sh
 	 
-	 # copy the generated package to the web server
+	 # Copy the generated package to the web server.
+
 	 echo 'Going to copy the package to the web server'
 	 scp -P $3 'buildbot:cadabra2/build/cadabra*' .
 	 scp cadabra*.$2 cadabra_web:/var/www/cadabra2/packages/
 	 
-	 # take down the VM gracefully
+	 # Take down the VM gracefully.
+
 	 echo 'Shutting down build bot...'
 	 ssh -tt -p $3 buildbot 'sudo shutdown now'
 	 
-	 # close the tunnel to the VM
+	 # Close the tunnel to the VM.
+
 	 ssh -S my-ctrl-socket -O exit buildbot
 }
 
+# To setup a new build bot, install a basic system, install
+# openssh-server, and setup a tunnel from a local port (to be given
+# below) to the ssh port on the build bot. Put
+#
+#   kasper ALL=(ALL) NOPASSWD: ALL
+#
+# at the bottom of the file edited with 'sudo visudo' (to avoid
+# scripts asking for passwords).
+#
+# Then install 'git', install all cadabra2 dependencies, and clone the
+# github repo into ~/cadabra2.
 
 #runbuild "Ubuntu_16.04_build" ".deb" 7000
-runbuild "Fedora_24_build" ".rpm" 7001
+#runbuild "Fedora_24_build" ".rpm" 7001
+runbuild "Mint_18" ".deb" 7002
+#runbuild "OpenSUSE_Leap" ".rpm" 7003
+#runbuild "CentOS_7" ".rpm" 7004
