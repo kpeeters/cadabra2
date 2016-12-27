3882197e21d8558f9319e969634f3f33ea0a417a
Kasper Peeters <kasper.peeters@phi-sci.com>
2016-12-27 21:34:07 +0000

    Incomplete fixes for unwrap

--- core/algorithms/unwrap.hh
+++ core/algorithms/unwrap.hh
@@ -3,9 +3,11 @@
 
 class unwrap : public Algorithm {
 	public:
-		unwrap(const Kernel&, Ex&);
+		unwrap(const Kernel&, Ex&, Ex&);
 		
 		virtual bool     can_apply(iterator) override;
 		virtual result_t apply(iterator&) override;
+
+		std::vector<Ex> wrappers;
 };
 
