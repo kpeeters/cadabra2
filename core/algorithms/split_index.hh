c464b63496045b8c63c8c7cc6446cc050448e37f
Kasper Peeters <kasper.peeters@phi-sci.com>
2017-01-20 12:45:34 +0000

    Allow for individual coordinates in split_index

--- core/algorithms/split_index.hh
+++ core/algorithms/split_index.hh
@@ -3,6 +3,7 @@
 
 #include "Algorithm.hh"
 #include "properties/Indices.hh"
+#include "properties/Coordinate.hh"
 
 class split_index : public Algorithm {
 	public:
@@ -10,9 +11,12 @@ class split_index : public Algorithm {
 
 		virtual bool     can_apply(iterator);
 		virtual result_t apply(iterator&);
-
+		
 	private:
 		const Indices *full_class, *part1_class, *part2_class;
+		const Coordinate *part1_coord, *part2_coord;
 		bool     part1_is_number, part2_is_number;
 		long     num1, num2;
+
+		iterator part1_coord_node, part2_coord_node;
 };
