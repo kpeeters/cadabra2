5ddffdfc8581003230545a2807d490eae92a475d
Kasper Peeters <kasper.peeters@phi-sci.com>
2018-02-24 20:37:34 +0000

    Fix unistd.h dependence

--- libs/internal/include/internal/unistd.h
+++ libs/internal/include/internal/unistd.h
@@ -2,14 +2,11 @@
 // Compatibility include to substitute for unistd.h on Windows.
 
 #ifdef _MSC_VER
-
-#include <io.h>
-#include <process.h>
-#include <direct.h>
-#define pid_t int
-
+  #include <io.h>
+  #include <process.h>
+  #include <direct.h>
+  #define pid_t int
+  #define sleep(x) Sleep(1000 * x)
 #else
-
-#include <unistd.h>
-
+  #include <unistd.h>
 #endif
