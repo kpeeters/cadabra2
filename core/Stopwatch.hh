b9a8446d324564870ac45b9ddeead57082329103
Kasper Peeters <kasper.peeters@phi-sci.com>
2018-02-24 12:28:14 +0000

    Make Stopwatch not depend on Posix anymore.

--- core/Stopwatch.hh
+++ core/Stopwatch.hh
@@ -21,35 +21,119 @@
 #ifndef stopwatch_hh__
 #define stopwatch_hh__
 
-extern "C" {
-#include <sys/time.h>
-#include <internal/unistd.h>
-#include <signal.h>
-}
-
+#include <chrono>
+#include <iosfwd>
 #include <iostream>
+
+/// \class Stopwatch
+/// \ingroup Client-Server
+///
+/// The Stopwach class provides a simple interace to allow
+/// timing function calls etc... It is possible to stop
+/// the stopwatch for an indefinite amount of time without
+/// losing the current elapsed time period, allowing it
+/// to be used to calculate the actual amount of time 
+/// a function spends performing a task without taking 
+/// into account e.g. time spent being blocked by a mutex.
+/// The class is exported to python in the cadabra2 
+/// module as Stopwatch.
+///
+///
+/// Example C++ usage:
+///
+/// #include <iostream>
+/// #include <vector>
+/// #include "Stopwatch.hh"
+///
+/// void do_other_stuff() { std::cout << "Doing other stuff\n"; }
+///
+/// int main()
+/// {
+///   const int n = 1000;
+///   std::vector<int> v;
+///   Stopwatch s;
+///
+///   s.start();
+///   for (unsigned int i = 0; i < n; ++i)
+///     v.push_back(i);
+///   s.stop();
+///   do_other_stuff();
+///   s.start();
+///   for (unsigned int i = n; i > 0; --i)
+///     v.push_back(i);
+///   s.stop();
+///   std::cout << "Total time spent pushing to vector and not spent doing other stuff: " << s << '\n';
+///
+///   s.reset();
+///   s.start();
+///   do_other_stuff();
+///   s.stop();
+///   std::cout << "Doing other stuff takes " << s.useconds() << "us\n"; //Assuming do_other_stuff() takes < 1s
+///   return 0;
+/// }
+///
+///
+/// Example python usage:
+///
+/// from cadabra2 import Stopwatch
+///
+/// def do_other_stuff():
+///   print("Doing other stuff")
+///
+/// n = 1000
+/// v = []
+/// s = Stopwatch()
+///
+/// s.start()
+/// for i in range(n):
+///   v += [i]
+/// s.stop()
+/// do_other_stuff()
+/// s.start()
+/// for i in range(n, 0, -1):
+///   v += [i]
+/// s.stop()
+/// print("Total time spent pushing to vector and not spent doing other stuff: {}".format(s))
+///
+/// s.reset()
+/// s.start()
+/// do_other_stuff()
+/// s.stop()
+/// print("Doing other stuff takes {}us".format(s.useconds())) #Assuming do_other_stuff() takes < 1s
+
 
 class Stopwatch {
 	public:
 		Stopwatch();
 
+      typedef std::chrono::steady_clock clock;
+
+      /// Reset to no-time-elapsed.
 		void reset();
+      /// Continue timing (does not reset).
 		void start();
+      /// Stop timing.
 		void stop();
+      /// Number of seconds elapsed.
 		long seconds() const;
+      /// Number of micro-seconds elapsed (needs to be added to 'seconds').
 		long useconds() const;
+      /// Is the stopwatch currently timing?
 		bool stopped() const;
 
 		friend std::ostream& operator<<(std::ostream&, const Stopwatch&);
 
 	private:
 		void checkpoint_() const;
-		mutable struct timeval  tv1,tv2; 
-		mutable struct timezone tz;
-		mutable long diffsec, diffusec;
-		bool stopped_;
+
+      mutable clock::time_point start_;
+      mutable long              elapsed_;
+		bool                      stopped_;
+
+      static const long s_to_us = 1000000L;
 };
 
+/// Print human-readable representation of the time elapsed.
 std::ostream& operator<<(std::ostream&, const Stopwatch&);
 
 #endif
