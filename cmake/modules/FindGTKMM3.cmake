b44292b8b472831189a6be746be12b4dbe700477
Kasper Peeters <kasper.peeters@phi-sci.com>
2018-03-06 19:50:12 +0000

    Try to link cadabra_client statically.

--- cmake/modules/FindGTKMM3.cmake
+++ cmake/modules/FindGTKMM3.cmake
@@ -10,7 +10,8 @@
 #    glibmm and its dependencies
 
 if(WIN32)
-  windows_find_library(GTKMM3 "gtkmm.dll" "glib;sigc++;pango")
+  #  windows_find_library(GTKMM3 "gtkmm.dll" "glib;sigc++;pango")
+  set(GTKMM3_LIBRARIES gtkmm.dll sigc-2.0.dll glib-2.dll gobject-2.dll glibmm.dll cairomm-1.0.dll pangomm.dll)
 else()
   find_package(PkgConfig REQUIRED)
   pkg_check_modules(GTKMM3 REQUIRED gtkmm-3.0)
