49feeb0bfe0f890ef6d64d00b2ac2047ab17bbf6
Dominic Price <dominicprice@outlook.com>
2018-07-23 16:25:00 +0100

    Finished changes for Ninja build support

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -5,3 +5,42 @@ macro(print_header TEXT)
 	message("  ${TEXT}")
 	message("-------------------------------------------")
 endmacro()
+
+# Install directory permissions
+macro(install_directory_permissions DIR)
+	install(
+		DIRECTORY DESTINATION ${DIR}
+		DIRECTORY_PERMISSIONS 
+		OWNER_READ 
+		OWNER_WRITE 
+		OWNER_EXECUTE 
+		GROUP_READ 
+		GROUP_EXECUTE 
+		WORLD_READ 
+		WORLD_EXECUTE
+	)
+endmacro()
+
+# Executes rm -f on FILENAME
+macro(remove_file FILENAME)
+	install(CODE "execute_process(COMMAND rm -f ${FILENAME})")
+endmacro()
+
+# Inserts an install directive to copy all dlls from
+# the build directory of SUBPROJECT to the Install
+# bin folder
+macro(install_dlls_from SUBPROJECT)
+	if(CMAKE_GENERATOR MATCHES "Visual Studio.*")
+		install(
+			DIRECTORY "${CMAKE_BINARY_DIR}/${SUBPROJECT}/${CMAKE_BUILD_TYPE}/"
+			DESTINATION bin
+			FILES_MATCHING PATTERN "*.dll"
+		)
+	else()
+		install(
+			DIRECTORY "${CMAKE_BINARY_DIR}/${SUBPROJECT}/"
+			DESTINATION bin
+			FILES_MATCHING PATTERN "*.dll"
+		)
+	endif()
+endmacro()
