41d5bef3376190d99f2f6c00905153bf5337475a
Kasper Peeters <kasper.peeters@phi-sci.com>
2018-08-14 18:06:26 +0200

    Fix warning about virtual function

--- core/properties/Tableau.hh
+++ core/properties/Tableau.hh
@@ -8,7 +8,7 @@ namespace cadabra {
 	class Tableau : public property {
 		public:
 			virtual ~Tableau() {};
-			virtual std::string name() const;
+			virtual std::string name() const override;
 	      virtual bool        parse(Kernel&, keyval_t& keyvals) override;
 			
 			int dimension;
