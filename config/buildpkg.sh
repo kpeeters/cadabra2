9de600b68290efdaffb16ca6673f407d3d961f6e
Kasper Peeters <kasper.peeters@phi-sci.com>
2019-04-08 22:02:53 +0100

    Fix buildbot

--- config/buildpkg.sh
+++ config/buildpkg.sh
@@ -9,17 +9,17 @@ git pull origin
 sudo rm -Rf build/*
 mkdir -p build
 cd build
-if [ -f /etc/redhat-release ]; then 
+if [ -f /etc/redhat-release ]; then
     centos="`cat /etc/redhat-release | grep CentOS`"
-	 scilin="`cat /etc/redhat-release | grep Scientific`"
+    scilin="`cat /etc/redhat-release | grep Scientific`"
     if [ -n "${centos}" -o -n "${scilin}" ]; then
-		  source /opt/rh/rh-python36/enable
-		  source /opt/rh/devtoolset-7/enable
-		  sudo alternatives --install /usr/bin/python python /usr/bin/python3.5 60
+        source /opt/rh/rh-python36/enable
+        source /opt/rh/devtoolset-7/enable
+        sudo alternatives --install /usr/bin/python python /usr/bin/python3.6 60
         cmake3 -DPACKAGING_MODE=ON -DCMAKE_INSTALL_PREFIX=/usr ..
     else
         cmake -DPACKAGING_MODE=ON -DCMAKE_INSTALL_PREFIX=/usr ..
-   fi
+    fi
 else
     cmake -DPACKAGING_MODE=ON -DCMAKE_INSTALL_PREFIX=/usr ..
 fi
