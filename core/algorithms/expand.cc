8a56e64b87cabe01150bf22dfffdf7baddd1d0ea
Connor Behan <connor.behan@gmail.com>
2019-10-22 15:20:42 +0100

    Make expand() respect index position
    For expand() to work on an index bracket, the factors need to have the
    ImplicitIndex property, sometimes inherited through Matrix. However,
    ImplicitIndex also allows a concrete convention of upper and lower
    indices to be specified. When this is present for all factors, we should
    use this to determine where new indices should go instead of hard coding
    subscripts.
    
    Signed-off-by: Connor Behan <connor.behan@gmail.com>

--- core/algorithms/expand.cc
+++ core/algorithms/expand.cc
@@ -11,6 +11,24 @@ expand::expand(const Kernel& k, Ex& e)
 	{
 	}
 
+index_iterator expand::nth_implicit_index(Ex::iterator eform, Ex::iterator iform, unsigned int n)
+	{
+	unsigned int hits=0;
+	index_iterator ch1=begin_index(eform);
+	while(ch1!=end_index(eform)) {
+		bool found=false;
+		index_iterator ch2=begin_index(iform);
+		while(!found && ch2!=end_index(iform)) {
+			if(*ch1->name==*ch2->name) found=true;
+			++ch2;
+			}
+		if(!found) ++hits;
+		if(hits==n) return ch1;
+		++ch1;
+		}
+	return ch1;
+	}
+
 bool expand::can_apply(iterator it)
 	{
 	if(*it->name=="\\indexbracket")
@@ -19,9 +37,20 @@ bool expand::can_apply(iterator it)
 			// or the last object should be the one with only one index. We
 			// do this by checking for the 'Matrix' property on the first and
 			// last element.
+			check_pos=false;
 			one_index=(tr.number_of_children(it)==2);
 			sibling_iterator prod=tr.begin(it);
 			sibling_iterator sib=tr.begin(prod);
+			sibling_iterator ind1=prod;
+			++ind;
+			sibling_iterator ind2=ind1;
+			if(!one_index) ++ind2;
+			const Indices *props=kernel.properties.get<Indices>(ind1, true);
+			// For this to stay true, every factor below must come with an
+			// index structure. If it is missing even once, we will not be
+			// able to guess what the user really meant.
+			if(props && props->position_type!=Indices::free) check_pos=true;
+
 			mx_first=tr.end();
 			mx_last=tr.end();
 			ii_first=tr.end();
@@ -30,8 +59,9 @@ bool expand::can_apply(iterator it)
 			unsigned int index_open=0;
 
 			while(sib!=tr.end(prod)) {
-				const ImplicitIndex *impi=kernel.properties.get_composite<ImplicitIndex>(sib);
-				if(impi) {
+				int tmp;
+				auto impi=kernel.properties.get_with_pattern<ImplicitIndex>(sib, tmp);
+				if(impi.first) {
 					const Matrix *mat=kernel.properties.get_composite<Matrix>(sib);
 					if(mat) {
 						if(index_open==0) {
@@ -39,6 +69,17 @@ bool expand::can_apply(iterator it)
 							index_open=2;
 							}
 						mx_last=sib;
+						// If an explicit form exists, verify that it has
+						// exactly two more indices than the implicit one.
+						if(impi.first->explicit_form.size()>0) {
+							Ex::iterator eform=impi.first->explicit_form.begin();
+							Ex::iterator iform=impi.second->obj.begin();
+							if(tr.number_of_children(eform)-tr.number_of_children(iform)!=2) {
+								throw ConsistencyException("Matrices should have two implicit indices.");
+								return false;
+								}
+							}
+						else check_pos = false;
 						}
 					else {
 						if(index_open==0) {
@@ -51,11 +92,44 @@ bool expand::can_apply(iterator it)
 							mx_last=tr.end();
 							--index_open;
 							}
+						// If an explicit form exists, verify that it has
+						// exactly one more index than the implicit one.
+						if(impi.first->explicit_form.size()>0) {
+							Ex::iterator eform=impi.first->explicit_form.begin();
+							Ex::iterator iform=impi.second->obj.begin();
+							if(tr.number_of_children(eform)-tr.number_of_children(iform)!=1) {
+								throw ConsistencyException("Vectors should have one implicit index.");
+								return false;
+								}
+							}
+						else check_pos=fase;
 						}
 					}
 				++sib;
 				}
 
+			if(check_pos && mx_first!=tr.end()) {
+				int tmp;
+				auto impi=kernel.properties.get_with_pattern<ImplicitIndex>(mx_first, tmp);
+				Ex::iterator eform=impi.first->explicit_form.begin();
+				Ex::iterator iform=impi.second->obj.begin();
+				index_iterator ch1=nth_implicit_index(eform, iform, 1);
+				if(ch1->fl.parent_rel!=ind1->fl.parent_rel) {
+					throw ConsistencyException("Bracket index structure is not compatible with the factors.");
+					return false;
+					}
+				}
+			if(check_pos && mx_last!=tr.end()) {
+				int tmp;
+				auto impi=kernel.properties.get_with_pattern<ImplicitIndex>(mx_last, tmp);
+				Ex::iterator eform=impi.first->explicit_form.begin();
+				Ex::iterator iform=impi.second->obj.begin();
+				index_iterator ch1=nth_implicit_index(eform, iform, 2);
+				if(ch1->fl.parent_rel!=ind2->fl.parent_rel) {
+					throw ConsistencyException("Bracket index structure is not compatible with the factors.");
+					return false;
+					}
+				}
 			if(index_open+1==tr.number_of_children(it)) return true;
 			}
 	return false;
@@ -79,9 +153,10 @@ Algorithm::result_t expand::apply(iterator& it)
 	sibling_iterator sib=tr.begin(prod);
 	Ex dum;
 	while(sib!=tr.end(prod)) {
-		const ImplicitIndex *impi=kernel.properties.get<ImplicitIndex>(sib);
+		int tmp;
+		auto impi=kernel.properties.get_with_pattern<ImplicitIndex>(sib, tmp);
 		sib->fl.bracket=str_node::b_none;
-		if(impi) {
+		if(impi.first) {
 			const Matrix *mat=kernel.properties.get_composite<Matrix>(sib);
 			sibling_iterator origobj=sib;
 
@@ -90,7 +165,13 @@ Algorithm::result_t expand::apply(iterator& it)
 			if(dum.size()>0) {
 				iterator tmpit=tr.append_child((iterator)(sib), dum.begin());
 				tmpit->fl.bracket=str_node::b_none;
-				tmpit->fl.parent_rel=str_node::p_sub;
+				if(check_pos) {
+					Ex::iterator eform=impi.first->explicit_form.begin();
+					Ex::iterator iform=impi.second->obj.begin();
+					index_iterator ch1=nth_implicit_index(eform, iform, 1);
+					tmpit->fl.parent_rel=ch->fl.parent_rel;
+					}
+				else tmpit->fl.parent_rel=str_node::p_sub;
 				}
 
 			if(mat) { // two-index object
@@ -106,7 +187,13 @@ Algorithm::result_t expand::apply(iterator& it)
 					dum=get_dummy(dums, sib);
 					iterator tmpit=tr.append_child((iterator)(sib), dum.begin());
 					tmpit->fl.bracket=str_node::b_none;
-					tmpit->fl.parent_rel=str_node::p_sub;
+					if(check_pos) {
+						Ex::iterator eform=impi.first->explicit_form.begin();
+						Ex::iterator iform=impi.second->obj.begin();
+						index_iterator ch1=nth_implicit_index(eform, iform, 2);
+						tmpit->fl.parent_rel=ch->fl.parent_rel;
+						}
+					else tmpit->fl.parent_rel=str_node::p_sub;
 					}
 				}
 			else {   // one-index object
@@ -114,7 +201,13 @@ Algorithm::result_t expand::apply(iterator& it)
 					dum=get_dummy(dums, sib);
 					iterator tmpit=tr.append_child((iterator)(sib), dum.begin());
 					tmpit->fl.bracket=str_node::b_none;
-					tmpit->fl.parent_rel=str_node::p_sub;
+					if(check_pos) {
+						Ex::iterator eform=impi.first->explicit_form.begin();
+						Ex::iterator iform=impi.second->obj.begin();
+						index_iterator ch1=nth_implicit_index(eform, iform, 1);
+						tmpit->fl.parent_rel=ch->fl.parent_rel;
+						}
+					else tmpit->fl.parent_rel=str_node::p_sub;
 					}
 				else dum.clear();
 				}
