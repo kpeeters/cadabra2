ee5ad2aaa9870466c461f2073ee3250260072504
Connor Behan <connor.behan@gmail.com>
2019-10-22 19:16:20 +0100

    Fixes for expand() (#179)
    * Make expand() respect index position
    
    For expand() to work on an index bracket, the factors need to have the
    ImplicitIndex property, sometimes inherited through Matrix. However,
    ImplicitIndex also allows a concrete convention of upper and lower
    indices to be specified. When this is present for all factors, we should
    use this to determine where new indices should go instead of hard coding
    subscripts.
    
    Signed-off-by: Connor Behan <connor.behan@gmail.com>
    
    * Add expand() test
    
    A quick verification that we get a lower index contracted with an upper
    index now.
    
    Signed-off-by: Connor Behan <connor.behan@gmail.com>
    
    * Fix egregious typos
    
    Some working lines were overwritten with non-working lines when I
    changed the tab settings.
    
    Signed-off-by: Connor Behan <connor.behan@gmail.com>

--- core/algorithms/expand.hh
+++ core/algorithms/expand.hh
@@ -13,8 +13,9 @@ namespace cadabra {
 			virtual result_t apply(iterator&) override;
 
 		private:
-			iterator mx_first, mx_last, ii_first, ii_last;
-			bool     one_index;
+			iterator       mx_first, mx_last, ii_first, ii_last;
+			bool           one_index, check_pos;
+			index_iterator nth_implicit_index(Ex::iterator eform, Ex::iterator iform, unsigned int n);
 		};
 
 	}
