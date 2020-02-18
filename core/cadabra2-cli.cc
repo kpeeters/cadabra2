865194d6ee6aa8c57df2ff0e9d9590f343306be8
Dominic Price <dominicprice@outlook.com>
2020-02-18 12:44:48 +0000

    Added command line arguments

--- core/cadabra2-cli.cc
+++ core/cadabra2-cli.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <regex>
 #include "CdbPython.hh"
 #include "cadabra2-cli.hh"
@@ -8,26 +10,38 @@
 namespace py = pybind11;
 using namespace py::literals;
 
+#define CHECK_BIT(var, pos) 
+#define NO_BANNER 1
+#define IGNORE_SEMICOLON 2
 
-Shell::Shell(int argc, char* argv[])
+Shell::Shell(unsigned int flags)
 	: ps1("> ")
 	, ps2(". ")
+	, flags(flags)
 {
+	restart();
+}
 
+void Shell::restart()
+{
+	guard = std::make_unique<py::scoped_interpreter>();
+	locals.clear();
+	is_running = true;
+	return_code = 0;
 }
 
 int Shell::interact()
 {
-	pybind11::scoped_interpreter guard;
-	locals.clear();
-
 	std::string module_path = cadabra::install_prefix() + "/share/cadabra2/python";
 
 	py::exec("import sys; sys.path.append('" + module_path + "')");
-	py::print("Cadabra " CADABRA_VERSION_FULL " (build " CADABRA_VERSION_BUILD " dated " CADABRA_VERSION_DATE ")"); 
-	py::print("Copyright (C) " COPYRIGHT_YEARS "  Kasper Peeters <kasper.peeters@phi-sci.com>"); 
 	py::eval_file(module_path + "/cadabra2_defaults.py");
-	py::print("Using SymPy version", py::eval("sympy.__version__"));
+
+	if (!(flags & NO_BANNER)) {
+		py::print("Cadabra " CADABRA_VERSION_FULL " (build " CADABRA_VERSION_BUILD " dated " CADABRA_VERSION_DATE ")"); 
+		py::print("Copyright (C) " COPYRIGHT_YEARS "  Kasper Peeters <kasper.peeters@phi-sci.com>"); 
+		py::print("Using SymPy version", py::eval("sympy.__version__"));
+	}
 
 	std::string curline;
 	is_running = true;
@@ -49,7 +63,23 @@ int Shell::interact()
 
 int Shell::run_script(const std::string& filename)
 {
-	return 1;
+	std::ifstream f(filename);
+	if (!f.is_open()) {
+		std::cout << "No file: " << filename << "\n";
+		return -1;
+	}
+	std::stringstream buf;
+	buf << f.rdbuf();
+	auto code = cadabra::cdb2python(buf.str(), !(flags & IGNORE_SEMICOLON));
+	try {
+		execute(code);
+	}
+	catch (py::error_already_set& err) {
+		std::string msg = err.what();
+		process_error(msg);
+		return (return_code == 0 ? -1 : return_code);
+	}
+	return 0;
 }
 
 void Shell::execute(const std::string& line)
@@ -60,8 +90,11 @@ void Shell::execute(const std::string& line)
 bool Shell::try_evaluate(const std::string& s)
 {
 	try {
+		py::print("About to evaluate: ", s);
 		auto res = py::eval(s, py::globals(), locals);
+		py::print("Evaluated!");
 		if (!res.is_none()) {
+			py::print("Result was not None, printing...");
 			py::print(res);
 			locals["_"] = res;
 		}
@@ -114,7 +147,7 @@ void Shell::process_ps2(const std::string& line)
 	}
 
 	try {
-		execute(cadabra::cdb2python_string(collect, true));
+		execute(cadabra::cdb2python_string(collect, !(flags & IGNORE_SEMICOLON)));
 		collect.clear();
 	}	
 	catch (py::error_already_set& err) {
@@ -179,9 +212,70 @@ void Shell::process_system_exit(const std::string& msg)
 	}
 }
 
+void usage()
+{
+
+}
 
 int main(int argc, char* argv[])
 {
-	Shell shell(argc, argv);
-	return shell.interact();
+	// Collect arguments
+	std::vector<std::string> opts, scripts;
+	bool accept_opts = true;
+	for (int i = 1; i < argc; ++i) {
+		char* arg = argv[i];
+		int len = strlen(arg);
+		if (len == 0)
+			continue;
+		if (arg[0] == '-') {
+			if (len == 1) {
+				accept_opts = false;
+			}
+			else if (arg[1] == '-') {
+				opts.emplace_back(arg + 2);
+			}
+			else {
+				for (int j = 1; j < len; ++j)
+					opts.emplace_back(1, arg[j]);
+			}
+		}
+		else {
+			scripts.emplace_back(arg);
+		}
+	}
+
+	unsigned int flags = 0;
+	bool force_interactive = false;
+	bool chain_scripts = false;
+
+	for (const auto& opt : opts) {
+		if (opt == "i" || opt == "interactive") {
+			force_interactive = true;
+		}
+		else if (opt == "q" || opt == "quiet") {
+			flags |= NO_BANNER;
+		}
+		else if (opt == "n" || opt == "no-display") {
+			flags |= IGNORE_SEMICOLON;
+		}
+		else if (opt == "h" || opt == "help") {
+			usage();
+			return 0;
+		}
+		else if (opt == "c" || opt == "chain") {
+			chain_scripts = true;
+		}
+	}
+
+	Shell shell(flags);
+
+	for (const auto& script : scripts) {
+		if (!chain_scripts)	
+			shell.restart();
+		int res = shell.run_script(script);
+		if (res != 0)
+			return res;
+	}
+	if (scripts.empty()|| force_interactive)
+		return shell.interact();
 }
--- core/cadabra2-cli.hh
+++ core/cadabra2-cli.hh
@@ -1,11 +1,13 @@
 #include <pybind11/embed.h>
 #include <string>
+#include <memory>
 
 class Shell
 {
 public:
-    Shell(int argc, char* argv[]);
+    Shell(unsigned int flags = 0);
 
+    void restart();
     int interact();
     int run_script(const std::string& filename);
 
@@ -20,9 +22,14 @@ private:
     void process_syntax_error(const std::string& msg);
     void process_system_exit(const std::string& msg);
 
+	pybind11::dict locals;
+    std::unique_ptr<pybind11::scoped_interpreter> guard;
+
     bool is_running;
 	int return_code;
-	pybind11::dict locals;
+
 	std::string ps1, ps2;
 	std::string collect;
+
+    unsigned int flags;
 };
