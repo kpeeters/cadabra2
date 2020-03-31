f88769737f29d628d3079c6f86e17d17539ec619
Kasper Peeters <kasper.peeters@phi-sci.com>
2020-03-31 19:41:56 +0100

    Fixes for conda build files.

--- conda/build.sh
+++ conda/build.sh
@@ -1,15 +1,16 @@
 # Conda build script to build a Cadabra Jupyter kernel.
 
+rm -Rf build
 mkdir build
 cd build
-cmake -G $CMAKE_GENERATOR \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_BUILD_TYPE=Release $SRC_DIR \
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_BUILD_TYPE=Release ${SRC_DIR} \
+		-DENABLE_MATHEMATICA=OFF \
       -DENABLE_JUPYTER=ON \
       -DENABLE_FRONTEND=OFF \
       -DCMAKE_INCLUDE_PATH=${HOME}/miniconda3/include \
       -DCMAKE_LIBRARY_PATH=${HOME}/miniconda3/lib \
-      -DCMAKE_INSTALL_PREFIX=${HOME}/miniconda3
+      ..
 
 cmake --build .
 cmake --build . --target install
