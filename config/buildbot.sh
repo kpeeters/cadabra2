343119d2cc781a2c2d109f8a9f56ab6daee59d1c
Kasper Peeters <kasper.peeters@phi-sci.com>
2020-07-25 23:03:33 +0100

    Make mathematica optional again.

--- config/buildbot.sh
+++ config/buildbot.sh
@@ -58,16 +58,16 @@ function runbuild {
 # Parameters: VM name, package type, local ssh port, folder name on web server.
 
 runbuild "Ubuntu_20.04"        ".deb" 7030 ubuntu2004
-runbuild "Ubuntu_18.04"        ".deb" 7017 ubuntu1804
-runbuild "Fedora_28"           ".rpm" 7020 fedora28
-runbuild "Fedora_29"           ".rpm" 7025 fedora29               
-runbuild "Mint_19"             ".deb" 7022 mint19
-#runbuild "OpenSUSE_15"         ".rpm" 7024 opensuse150  ssh error
-#runbuild "OpenSUSE_Tumbleweed" ".rpm" 7023 opensusetw   ssh error
-#runbuild "CentOS_7"            ".rpm" 7004 centos7      cmake/packaging clash
-#runbuild "Scientific_Linux_74" ".rpm" 7013 scientific7x
-runbuild "Debian_921"          ".deb" 7014 debian9
-runbuild "Debian_Buster"       ".deb" 7021 debian10
+#runbuild "Ubuntu_18.04"        ".deb" 7017 ubuntu1804
+#runbuild "Fedora_28"           ".rpm" 7020 fedora28
+#runbuild "Fedora_29"           ".rpm" 7025 fedora29               
+#runbuild "Mint_19"             ".deb" 7022 mint19
+# runbuild "OpenSUSE_15"         ".rpm" 7024 opensuse150  ssh error
+# runbuild "OpenSUSE_Tumbleweed" ".rpm" 7023 opensusetw   ssh error
+# runbuild "CentOS_7"            ".rpm" 7004 centos7      cmake/packaging clash
+# runbuild "Scientific_Linux_74" ".rpm" 7013 scientific7x
+#runbuild "Debian_921"          ".deb" 7014 debian9
+#runbuild "Debian_Buster"       ".deb" 7021 debian10
 
 # Outdated versions:
 # runbuild "Mint_18" ".deb" 7002 mint18                   ERROR
