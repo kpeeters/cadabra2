7eeb4de9475f892c0ecd2042697e1c7263b91f65
Kasper Peeters <kasper.peeters@phi-sci.com>
2020-09-18 21:54:47 +0100

    Fix incomplete line.

--- frontend/common/TeXEngine.cc
+++ frontend/common/TeXEngine.cc
@@ -468,7 +468,7 @@ void TeXEngine::convert_set(std::set<std::shared_ptr<TeXRequest> >& reqs)
 	// a 'latex' command anymore which supports all these options (e.g. MikTeX).
 
 	std::string latex_stdout, latex_stderr;
-	tpl::Process latex_proc("pdflatex --quiet --halt-on-error --output-format=dvi "+nf, "",
+	tpl::Process latex_proc("pdflatex --halt-on-error --output-format=dvi "+nf, "",
 	[&](const char *bytes, size_t n) {
 		latex_stdout=std::string(bytes,n);
 		},
