a51b689fd01a8b598108ac6282a9ebb8c323fa36
Dominic Price <dominicprice@hotmail.co.uk>
2020-11-10 18:27:30 +0000

    Redirect stdout and stderr on windows release build

--- frontend/gtkmm/main.cc
+++ frontend/gtkmm/main.cc
@@ -31,24 +31,46 @@ int main(int argc, char **argv)
 	}
 
 #if defined(_WIN32) && defined(NDEBUG)
+
+void display_error(const std::string& message)
+{
+	MessageBoxA(NULL, message.c_str(), "Error!", MB_ICONEXCLAMATION | MB_OK);
+}
+
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 	{
+	if (getenv("CADABRA2_LOG_OUTPUT") != NULL) {
+		auto stdout_path = Glib::get_user_data_dir() + "/cadabra2-stdout.log";
+		if (freopen(stdout_path.c_str(), "w", stdout) == NULL) {
+			display_error("Could not redirect stdout to " + stdout_path);
+			exit(1);
+		}
+
+		auto stderr_path = Glib::get_user_data_dir() + "/cadabra2-stderr.log";
+		if (freopen(stderr_path.c_str(), "w", stderr) == NULL) {
+			display_error("Could not redirect stdout to " + stderr_path);
+			exit(1);
+		}
+
+		std::ios_base::sync_with_stdio();
+	}
+
 	try {
 		auto application = Cadabra::create(__argc, __argv);
 		const int status = application->run();
 		return status;
 		}
 	catch (Glib::Error& er) {
-		Gtk::MessageDialog(er.what()).run();
+		display_error(er.what());
 		return -1;
 		}
 	catch (std::exception& ex) {
-		Gtk::MessageDialog(ex.what()).run();
+		display_error(ex.what());
 		return -1;
 		}
 	catch (...) {
-		Gtk::MessageDialog("An unknown error occured :(").run();
-		throw;
+		display_error("An unknown error occurred");
+		return -1;
 		}
 	}
 #endif
