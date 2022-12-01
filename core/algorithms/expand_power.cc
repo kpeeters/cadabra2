a4cedb180044d8a7321d169b250d5c058c3d5760
Kasper Peeters <kasper.peeters@cadabra.science>
2022-12-01 21:16:21 +0000

    Make expand_power work on negative powers.

--- core/algorithms/expand_power.cc
+++ core/algorithms/expand_power.cc
@@ -27,9 +27,12 @@ Algorithm::result_t expand_power::apply(iterator& it)
 	++exponent;
 
 	int num=to_long(*exponent->multiplier);
-	if(num<1)
-		return result_t::l_no_action;
+//	if(num<1)
+//		return result_t::l_no_action;
 
+	if(num==-1 && *it->name!="\\prod")
+		return result_t::l_no_action;
+	
 	iterator prodn=tr.insert(argument,str_node("\\prod"));
 
 	// If the current \pow is inside a sum, do not discard the bracket
@@ -37,18 +40,24 @@ Algorithm::result_t expand_power::apply(iterator& it)
 	if(tr.is_head(it)==false && *tr.parent(it)->name=="\\sum")
 		prodn->fl.bracket=it->fl.bracket;
 
+	// Two cases:
+	//    (a b c)**2 -> a b c a b c;
+	//    (a b c)**(-2) -> a**(-1) b**(-1) c**(-1) a**(-1) b**(-1) c**(-1)
+	// So we treat the latter as the former, and then at the end
+	// just wrap all factors in a \pow{...}{-1}.
+	
 	sibling_iterator beg=argument;
 	sibling_iterator nd=beg;
 	++nd;
-	argument=tr.reparent(prodn,beg,nd);
+	argument=tr.reparent(prodn, beg, nd);
 	tr.erase(exponent);
 	tr.flatten(it);
 	multiply(prodn->multiplier, *it->multiplier);
 	it=tr.erase(it);
-
-	// Now duplicate the factor num-1 times.
+	
+	// Now duplicate the factor abs(num)-1 times.
 	multiplier_t tot=*argument->multiplier;
-	for(int i=0; i<num-1; ++i) {
+	for(int i=0; i<std::abs(num)-1; ++i) {
 		iterator tmp=tr.append_child(prodn);
 		tot *= *argument->multiplier;
 		iterator ins=tr.replace(tmp, argument);
@@ -56,9 +65,33 @@ Algorithm::result_t expand_power::apply(iterator& it)
 		rename_replacement_dummies(ins);
 		}
 	one(argument->multiplier);
-	multiply(prodn->multiplier, tot);
+	if(num<1)
+		multiply(prodn->multiplier, 1/tot);
+	else
+		multiply(prodn->multiplier, tot);
 
+
+	// If the argument of the original \pow{...}{...} was a \prod,
+	// then we now have a nested product.
 	cleanup_dispatch(kernel, tr, it);
+
+	if(num<1) {
+		if(*it->name=="\\prod") {
+			sibling_iterator sib = tr.begin(it);
+			while(sib!=tr.end(it)) {
+				auto nxt=sib;
+				++nxt;
+				auto newpow = tr.wrap(sib, str_node("\\pow"));
+				tr.append_child(newpow, str_node("1"))->multiplier = rat_set.insert(-1).first;
+				sib=nxt;
+				}
+			}
+		else {
+			auto newpow = tr.wrap(it, str_node("\\pow"));
+			tr.append_child(newpow, str_node("1"))->multiplier = rat_set.insert(-1).first;
+			}
+		}
+
 
 	return result_t::l_applied;
 	}
