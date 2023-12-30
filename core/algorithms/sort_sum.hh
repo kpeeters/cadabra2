6f511a7c8033b4b1af26d554921765ebe581eda8
Daniel Butter <dbutter@gmail.com>
2023-12-29 22:09:52 -0600

    Simplified sort_sum to just use std::stable_sort.

--- core/algorithms/sort_sum.hh
+++ core/algorithms/sort_sum.hh
@@ -7,21 +7,14 @@ namespace cadabra {
 
 	class sort_sum : public Algorithm {
 		public:
-			sort_sum(const Kernel&, Ex&, unsigned int runsize = 32);
+			sort_sum(const Kernel&, Ex&);
 
 			virtual bool     can_apply(iterator) override;
 			virtual result_t apply(iterator&) override;
 
 		private:
-			bool should_swap(iterator obj, int subtree_comparison) const;
+			//bool should_swap(iterator obj, int subtree_comparison) const;
 			bool should_swap(iterator obj1, iterator obj2, int subtree_comparison) const;
-			unsigned int runsize;
-			bool insertionSort(sibling_iterator, unsigned int);
-			bool timSort(iterator&);
-			bool merge(sibling_iterator, unsigned int, unsigned int, unsigned int);
-			unsigned int calcMinRun(unsigned int);
-
-
 
 		};
 
