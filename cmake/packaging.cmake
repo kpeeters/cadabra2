b9fe6727c374b1319972e30fd1f7aed064a6dc33
Kasper Peeters <kasper.peeters@cadabra.science>
2024-09-24 15:35:19 +0100

    Also copy deps of pixbufloader dlls.

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -204,9 +204,6 @@ if(WIN32)
   set(CPACK_GENERATOR NSIS)
   set(CPACK_PACKAGE_INSTALL_DIRECTORY "Cadabra")
   set(CPACK_NSIS_MODIFY_PATH ON)
-  set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS
-    "$INSTDIR\\\\gdk-pixbuf-query-loaders --update-cache"
-  )
   set(CPACK_NSIS_EXECUTABLES_DIRECTORY .)
 endif()
 
