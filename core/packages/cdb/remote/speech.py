515cb093cdd6a9d2e747b488b454b5572829f1f3
Kasper Peeters <kasper.peeters@cadabra.science>
2025-03-18 20:36:18 +0000

    Change to use OpenAI api for speech.

--- core/packages/cdb/remote/speech.py
+++ core/packages/cdb/remote/speech.py
@@ -1,31 +1,55 @@
 
-from piper.voice import PiperVoice
-from pydub import AudioSegment
-from pydub.playback import play
 import io
 import wave
 import time
 import tempfile
 import cdb.remote.highlight
+import openai
+import subprocess
+import time
 
 initialised=False
-model = None
-warping = False
+model    = None
+warping  = False
+client   = None
+say_proc = None
+say_num  = 0
 
-def init(voice_file="en_GB-alba-medium.onnx"):
-    global model, initialised
-    model = PiperVoice.load(voice_file, voice_file+".json")
-    initialised=True
+def init(url="http://localhost:8880/v1"):
+    global client
+    client=openai.OpenAI(base_url=url, api_key="not-needed")
 
 def warp(w=True):
     global warping
     warping=w
 
+def tts(txt, fname):
+    # print(f"calling api for {txt}...")
+    with client.audio.speech.with_streaming_response.create(
+            model="kokoro",
+            speed=1.1,
+            voice="bm_fable", #single or multiple voicepack combo
+            input=txt
+    ) as response:
+        response.stream_to_file(fname)
+        
+def say_async(fname, delay):
+    global say_proc
+    # print(f"playing...")
+    say_proc = subprocess.Popen(["bash", "-c", f"sleep {delay} && mpv {fname}"],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+
 def say(text, subtitle=True, subtext="", sleep=1):
-    global model, warping, initialised
+    """
+    Say a text. This will do the text-to-speech, then play the audio
+    async. So it returns immediately after the playing starts.
+    The async task will play silence for the indicated delay first.
+    """
+    global model, warping, say_num, say_proc
     
-    if not initialised:
-        raise Exception("First call cdb.videos.init(onnx_filename) to initialise.")
+    if client == None:
+        init()
 
     if subtitle:
         if subtext!="":
@@ -34,17 +58,18 @@ def say(text, subtitle=True, subtext="", sleep=1):
             cdb.remote.highlight.subtitle(text)
 
     if not warping:
-       tmp = tempfile.NamedTemporaryFile()
-       wave_file = wave.open(tmp.name, "w")
-       model.synthesize(text, wave_file)
-       wave_file.close()
-       audio_data = AudioSegment.from_wav(tmp.name)
-       play(audio_data)
-       time.sleep(sleep)
+        say_num += 1
+        fname = f"output_{say_num}.mp3"
+        tts(text, fname)
+        if say_proc != None:
+            say_proc.wait()
+        say_async(fname, sleep)
        
     if subtitle:
         cdb.remote.highlight.subtitle()
 
 if __name__ == "__main__":
-    say("Hello!")
-    say("We are going to have some fun with this.")
+    say("Hello!", subtitle=False)
+    say("We are going to have some fun with this.", subtitle=False)
+    say_proc.wait()
+    
